31eb6f3146d6aafdca33d0a3ab91299d781a7a87
Ari Susanto <risusanto@outlook.com>
2018-06-06 04:19:19 +0700

    initial commit

---
+++ dijkstra.py
@@ -0,0 +1,31 @@
+def dijkstra(graph,src):
+    length = len(graph)
+    nodes = [i for i in range(length)]
+
+    visited = [src]
+    path = {src:{src:[]}}
+    nodes.remove(src)
+    distance_graph = {src:0}
+    pre = next = src
+
+    while nodes:
+        distance = float('inf')
+        for v in visited:
+             for d in nodes:
+                new_dist = graph[src][v] + graph[v][d]
+                if new_dist <= distance:
+                    distance = new_dist
+                    next = d
+                    pre = v
+                    graph[src][d] = new_dist
+
+
+        path[src][next] = [i for i in path[src][pre]]
+        path[src][next].append(next)
+
+        distance_graph[next] = distance
+
+        visited.append(next)
+        nodes.remove(next)
+
+    return distance_graph, path
---
+++ main.py
@@ -0,0 +1,17 @@
+from dijkstra import dijkstra
+
+graph_list = [   
+    [0, 50, 10, 40, 45, float('inf')],
+    [float('inf'), 0, 15, float('inf'), 10, float('inf')],
+    [20, float('inf'), 0, 15, float('inf'), float('inf')],
+    [float('inf'), 20, float('inf'), 0, 35, float('inf')],
+    [float('inf'), float('inf'), float('inf'), 30, 0, float('inf')],
+    [float('inf'), float('inf'), float('inf'), 3, float('inf'), 0]
+    ]
+
+
+src = 0
+distance, path = dijkstra(graph_list, src)
+for key, value in distance.items():
+    print((src + 1), " ke ", (key + 1),"adalah ",
+    [x + 1 for x in ([src]+path[src][key])], " dengan panjang ", value)
